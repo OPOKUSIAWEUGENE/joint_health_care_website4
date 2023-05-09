11a4b844d958457233d9d40ab956219ac99f5f03
Tettey Divine <98411773+OPOKUSIAWEUGENE@users.noreply.github.com>
2023-05-09 18:11:42 +0000

    workign on the application form interface

--- src/NewApplication/Application.js
+++ src/NewApplication/Application.js
@@ -49,34 +49,125 @@ export default function Application(){
       const data=[
           {
             input:{
-                    type:"text", placeholder:"Enter Firstname",name:"firstname", value:"",
+                    label:"Applicant's First Name",type:"text", placeholder:"Enter Firstname",name:"firstname", value:"",
                     validation:{required:true,  maxLength:50, pattern:/[a-zA-Z]$/}
                   }
           },
           {
             input:{
-                    type:"text", placeholder:"Enter Lastname",name:"lastname", value:"",
+              label:"Applicant's Last Name", type:"text", placeholder:"Enter Lastname",name:"lastname", value:"",
                     validation:{required:true,  maxLength:50, pattern:/[a-zA-Z]$/}
                   }
           },
           {
             input:{
-                    type:"email", placeholder:"Enter Email",name:"email", value:"",
+              label:"Email", type:"text", placeholder:"Enter Email",name:"email", value:"",
                     validation:{required:true,  maxLength:50, pattern:/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/}
                   }
           },
           {
             input:{
-                    type:"text", placeholder:"Enter City/State/Region name",name:"city_state_region", value:"",
+              label:"Present Address", type:"text", placeholder:"Enter Present Address",name:"address", value:"",
                     validation:{required:true,  maxLength:50, pattern:/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/}
                   }
           },
           {
             input:{
-                    type:"password",name:"password", value:"", placeholder:"Enter Password",
-                    validation:{required:true,  maxLength:50 }
+              label:"City/Region/State", type:"text", placeholder:"Enter City/State/Region ",name:"city_state_region", value:"",
+                    validation:{required:true,  maxLength:50, pattern:/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/}
                   }
-          },   
+          },
+          {
+            input:{
+              label:"Phone", type:"telephone", placeholder:"Enter Phone Number ",name:"phonennnnnn", value:"",
+                    validation:{required:true,  maxLength:50, pattern:/[0-9]$/}
+                  }
+          },
+          {
+            select:{
+              label:"Are you above 18 years?",
+                    type:"select",name:"is_eighteen", defaultvalue:"yes",
+                    validation:{required:false,  Min:8 },
+                    options:[ 
+                              {value:"yes", label:"YES"},
+                              {value:"no", label:"NO"},      
+                           ],
+                        //   defaultvalue:true           
+                  }
+          },
+          {
+            select:{
+              label:"Business type?",
+                    type:"type",name:"type", defaultvalue:"full_time",
+                    validation:{required:false,  Min:8 },
+                    options:[ 
+                              {value:"full_time", label:"Full Time"},
+                              {value:"part_time", label:"PART TIME"}, 
+                              {value:"day", label:"DAY"},
+                              {value:"night", label:"NIGHT"},       
+                           ],
+                        //   defaultvalue:true           
+                  }
+          },
+
+          {
+            select:{
+             label:"Positionn",
+                   type:"select",name:"is_eighteen", defaultvalue:"yes",
+                   validation:{required:false,  Min:8 },
+                   options:[ 
+                             {value:"yes", label:"YES"},
+                             {value:"no", label:"NO"},      
+                          ],
+                       //   defaultvalue:true           
+                 }
+         },
+         {
+          select:{
+           label:"If you are not a US citizen, do you have the legal rights to remain permanentlly in the US?",
+                 type:"select",name:"is_eighteen", defaultvalue:"yes",
+                 validation:{required:false,  Min:8 },
+                 options:[ 
+                           {value:"yes", label:"YES"},
+                           {value:"no", label:"NO"},      
+                        ],
+                     //   defaultvalue:true           
+               }
+       },
+         
+        {
+          input:{
+            label:"Salary Requirement ", type:"text", placeholder:"Enter Salary ",name:"salary_requirement", value:"",
+                  validation:{required:true,  pattern:/[0-9]$/}
+                }
+        },
+          {
+            input:{
+              label:"Date Available", type:"date", placeholder:"Enter Date ",name:"date", value:"",
+                    validation:{required:true,  pattern:/[0-9]$/}
+                  }
+          },
+      
+        {
+          input:{
+            label:"Date Available ", type:"select", placeholder:"Enter Age ",name:"city_state_region", value:"",
+                  validation:{required:true,  pattern:/[0-9]$/}
+                }
+        },
+      
+        {
+          select:{
+           label:"Do you have adequate transportation to get to <br> work on time each day and when called on short notice during normal working hours?",
+                 type:"select",name:"have_adequate_transport", defaultvalue:"yes",
+                 validation:{required:false,  Min:8 },
+                 options:[ 
+                           {value:"yes", label:"YES"},
+                           {value:"no", label:"NO"},      
+                        ],
+                     //   defaultvalue:true           
+               }
+       },
+        
           {
             submit:{
                     type:"submit", value:loading?"Loading...":"Apply"
@@ -86,7 +177,9 @@ export default function Application(){
     return(
       <div className={styles.applicationPage} >
         <div className={styles.formHeight}> 
+       
         <div className={styles.form}>  
+        <div className={styles.formHeader}>APPLICATION FORM</div>
  <InputForm  data={data} onSubmit={Onsubmit}  />
         </div>
         </div>
