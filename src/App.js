efd76c80fc5de77c68159c95a0fc2a59b1a814d7
Eugene Opoku Siaw <opoku.s.eugene@gmail.com>
2026-05-18 22:42:47 +0000

    `Refactored CSS and JavaScript files with various changes`

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
-import Layout from './Layout';
-
-import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import NewApplication from './NewApplication/NewApplication'
-import './App.css';
-import SentApplication from './NewApplication/SentApplication';
+import Layout from "./Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewApplication from "./NewApplication/NewApplication";
+import "./App.css";
+import SentApplication from "./NewApplication/SentApplication";
 
 function App() {
   return (
-   
     <Router>
-<Routes>       
-<Route path="/" element={ <Layout replace to="/home" />} />
-<Route path="/new-application" element={ <NewApplication replace to="/new-application" />} />
-<Route path="/sent-application" element={ <SentApplication replace to="/sent-application" />} />
-</Routes>
-</Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/home" element={<Layout />} />
+        <Route path="/new-application" element={<NewApplication />} />
+        <Route path="/sent-application" element={<SentApplication />} />
+        <Route path="*" element={<Layout />} />
+      </Routes>
+    </Router>
   );
 }
 
